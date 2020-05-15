b3f3cded2fef7a3767942f4f5801ffd53a9361c3
lishuang <shuang11>
2020-05-15 18:40:24 +0800

    测试

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -3,6 +3,9 @@ package com.sunnyweather.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * 彩云天气
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
